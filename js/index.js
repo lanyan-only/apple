d84443a36b349f39e64e5e015ebfd2eba52d009e
lanyan-only <894468157@qq.com>
2016-03-21 17:45:22 +0800

    苹果

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+$(".big-search-label").mouseover(function(){
+  $(".shousuo").css("opacity",0.5)
+})
+$(".big-search-label").mouseout(function(){
+  $(".shousuo").css("opacity",1)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
